b5a0d805e35d45a0ee323eb2b08d7137a1e69762
Siyu Zhou <siyu.zhou@outlook.com>
2020-09-26 18:59:15 -0700

    Add suffix to save files.

--- examples/obstacle_avoidance_sim.py
+++ examples/obstacle_avoidance_sim.py
@@ -135,9 +135,9 @@ def main():
     timeseries_data_all, edge_data_all, time_data_all = \
         utils.run_simulation(simulation, ARGS, ARGS.instances, ARGS.processes, ARGS.batch_size)
 
-    np.save(os.path.join(ARGS.save_dir, ARGS.prefix + '_timeseries.npy'), timeseries_data_all)
-    np.save(os.path.join(ARGS.save_dir, ARGS.prefix + '_edge.npy'), edge_data_all)
-    np.save(os.path.join(ARGS.save_dir, ARGS.prefix+'_time.npy'), time_data_all)
+    np.save(os.path.join(ARGS.save_dir, f'{ARGS.prefix}_timeseries{ARGS.suffix}.npy'), timeseries_data_all)
+    np.save(os.path.join(ARGS.save_dir, f'{ARGS.prefix}_edge{ARGS.suffix}.npy'), edge_data_all)
+    np.save(os.path.join(ARGS.save_dir, f'{ARGS.prefix}_time{ARGS.suffix}.npy'), time_data_all)
 
 
 if __name__ == '__main__':
@@ -165,6 +165,8 @@ if __name__ == '__main__':
                         help='name of the save directory')
     parser.add_argument('--prefix', type=str, default='',
                         help='prefix for save files')
+    parser.add_argument('--suffix', type=str, default='',
+                        help='suffix for save files')
     parser.add_argument('--processes', type=int, default=1,
                         help='number of parallel processes')
     parser.add_argument('--batch-size', type=int, default=100,
